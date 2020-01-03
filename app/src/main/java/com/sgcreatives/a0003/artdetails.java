3789d515a3717f303ed2a49d530690943f0da7c0
daines chacko <daineschacko05@gmail.com>
2020-01-03 11:07:07 +0530

    ui complete

---
+++ app/src/main/java/com/sgcreatives/a0003/artdetails.java
@@ -0,0 +1,13 @@
+package com.sgcreatives.a0003;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class artdetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_artdetails);
+    }
+}
